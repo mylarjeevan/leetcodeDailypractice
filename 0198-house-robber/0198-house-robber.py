4adc4e727c162fac7d7fb22ef0089b063379ba18
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-07-15 15:41:06 +0530

    Time: 0 ms (100%), Space: 12.6 MB (1.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,20 @@
 class Solution(object):
-
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        size=len(nums)
-        dp={}
-        def func(nums,n):
+        n=len(nums)
+        if n==0:
+            return 0
+        if n==1:
+            return nums[0]
+        dp=[0]*(n)
+        dp[0]=nums[0]
+        dp[1]=max(dp[0],nums[1])
+        for i in range(2,n):
+            pick=nums[i]+dp[i-2]
+            not_pick=dp[i-1]
+            dp[i]=max(pick,not_pick)
+        return dp[n-1]
         
-            if n==0:
-                return nums[n]
-            if n<0:
-                return 0
-            if n in dp:
-                return dp[n]
-            pick=nums[n]+func(nums,n-2)
-            not_pick=0+func(nums,n-1)
-            dp[n]=max(pick,not_pick)
-            return dp[n]
-
-        return func(nums,size-1)
