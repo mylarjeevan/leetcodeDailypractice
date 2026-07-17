633351e5ab0f5837d2acc1de70654232efe7a5b3
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-07-17 16:29:01 +0530

    Time: 14 ms (10.32%), Space: 23.4 MB (16.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,13 +6,14 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
+       
         k=k%n
-        def reverse(left,right):
+        def change(left,right):
             while left<right:
                 nums[left],nums[right]=nums[right],nums[left]
                 left+=1
                 right-=1
-        reverse(0,n-1)
-        reverse(0,k-1)
-        reverse(k,n-1)
+        change(0,n-1)
+        change(0,k-1)
+        change(k,n-1)
         return nums
