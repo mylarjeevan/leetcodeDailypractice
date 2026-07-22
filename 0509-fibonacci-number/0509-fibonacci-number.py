2da8d559eab6769580d4f82497ee3aab3c826b34
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-07-22 20:02:05 +0530

    Time: 602 ms (35.09%), Space: 12.4 MB (52.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,14 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<1:
+        if n<=1:
             return n
-        prev2=0
-        prev1=1
-        for i in range(2,n+1):
-            curr=prev1+prev2
-            prev2=prev1
-            prev1=curr
-        return prev1
-
+        return self.fib(n-1)+self.fib(n-2)
         
