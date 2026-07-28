e034739d95e26ba0550121bbe6f1275bb86ae34b
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-07-28 18:05:57 +0530

    Time: 7 ms (98.31%), Space: 13 MB (90.44%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -1,36 +1,30 @@
-'''class Solution(object):
-    def maximumProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        numed=sorted(nums[:3],reverse=True)
-        first=numed[0]
-        second=numed[1]
-        third=numed[2]
-        for i in range(3,len(nums)):
-            if nums[i]>=first:
-                third=second
-                second=first
-                first=nums[i]
-            elif nums[i]<first and nums[i]>=second:
-                third=second
-                second=nums[i]
-            elif nums[i]<second and nums[i]>third:
-                third=nums[i]
-        return first*second*third
-        
-'''
-
 class Solution(object):
     def maximumProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n=len(nums)
-        nums.sort(reverse=True)
-        val1=nums[0]*nums[1]*nums[2]
-        val2=nums[0]*nums[n-1]*nums[n-2]
-        return max(val1,val2)
-        
+        # Three largest numbers
+        first = second = third = float('-inf')
+
+        # Two smallest numbers
+        small1 = small2 = float('inf')
+
+        for num in nums:
+
+            # Update three largest
+            if num > first:
+                third = second
+                second = first
+                first = num
+            elif num > second:
+                third = second
+                second = num
+            elif num > third:
+                third = num
+
+            # Update two smallest
+            if num < small1:
+                small2 = small1
+                small1 = num
+            elif num < small2:
+                small2 = num
+
+        return max(first * second * third,
+                   first * small1 * small2)
