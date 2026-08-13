6e71e2b985f999d36fcc749d2a1a1ba3deaf45fd
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-08-13 15:12:48 +0530

    Time: 0 ms (100%), Space: 12.5 MB (23.6%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def robs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        prev2=nums[0]
+        prev1=max(prev2,nums[1])
+        for i in range(2,n):
+            pick=nums[i]+prev2
+            not_pick=prev1
+            curr=max(pick,not_pick)
+            prev2=prev1
+            prev1=curr
+        return prev1
+    
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        return max(self.robs(nums[1:]),self.robs(nums[:-1]))
+        
