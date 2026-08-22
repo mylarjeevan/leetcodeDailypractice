61f984e54c320b426ed93e160f94532f11999786
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-08-22 14:24:33 +0530

    Time: 24 ms (35.15%), Space: 12.5 MB (67.44%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        s_dict=Counter(s)
+        t_dict=Counter(t)
+        return s_dict==t_dict
+        
