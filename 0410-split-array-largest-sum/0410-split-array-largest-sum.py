9b5ba584772e17642091603fefcc53e56335fdef
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-08-29 21:26:31 +0530

    Time: 7 ms (9.04%), Space: 12.4 MB (81.67%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def splitArray(self, arr, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(arr)
+        if k > n:
+            return -1
+        left=max(arr)
+        right=sum(arr)
+        ans=right
+        
+        def finder(arr,pages):
+            st=1
+            pagesstudent=0
+            for i in range(len(arr)):
+                if(pagesstudent+arr[i]<=pages):
+                    pagesstudent+=arr[i]
+                else:
+                    st+=1
+                    pagesstudent=arr[i]
+            return st
+            
+        while left<=right:
+            mid=left+(right-left)//2
+            if finder(arr,mid)>k:
+                left=mid+1
+            else:
+                ans=min(ans,mid)
+                right=mid-1
+        return ans
+            
+            
