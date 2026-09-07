c5ebc5b0355574f05b7eca6cef9cbcffcc88cdea
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-09-07 20:47:52 +0530

    Time: 55 ms (28.33%), Space: 56.1 MB (80.58%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,20 @@
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            current++;
+            return current;
+        },
+
+        decrement: function() {
+            current--;
+            return current;
+        },
+
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
