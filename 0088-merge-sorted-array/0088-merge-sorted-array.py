e91fd0b0497668abe0feb31db29a119b41d9c05d
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-09-09 18:46:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (21.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,18 +1,15 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-        if n==0:
-            return nums1
-        val=m+n
-        for i in range(m,len(nums1)):
-            nums1[i]=nums2[val-i-1]
-        nums1.sort()
-        return nums1
+        left = m - 1
+        right = n - 1
+        pointer = m + n - 1
 
-        
+        while right >= 0:
+            if left >= 0 and nums1[left] > nums2[right]:
+                nums1[pointer] = nums1[left]
+                left -= 1
+            else:
+                nums1[pointer] = nums2[right]
+                right -= 1
+
+            pointer -= 1
