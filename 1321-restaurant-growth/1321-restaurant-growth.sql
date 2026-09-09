ca2a1da32944083b85edea241fd40f95149dafa3
mylar jeevan <156415420+mylarjeevan@users.noreply.github.com>
2026-09-09 21:41:16 +0530

    Time: 401 ms (47.64%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH daily AS (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+
+SELECT 
+    d1.visited_on,
+    SUM(d2.amount) AS amount,
+    ROUND(SUM(d2.amount) / 7, 2) AS average_amount
+FROM daily d1
+JOIN daily d2
+    ON DATEDIFF(d1.visited_on, d2.visited_on) BETWEEN 0 AND 6
+GROUP BY d1.visited_on
+HAVING COUNT(d2.visited_on) = 7
+ORDER BY d1.visited_on;
